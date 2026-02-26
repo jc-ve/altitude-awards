bef7d724c9337dc24641ef8ffe1f888c4074caeb
jc-ve <john.christopher.enriquez.2@gmail.com>
2026-02-27 03:11:35 +0800

    initial

---
+++ src/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const categoryBtns = document.querySelector(".categories-btns");
+const categoryBtn = document.querySelectorAll(".category-btn");
+const categoriesContent = document.querySelectorAll(".categories");
+
+if (window.location.pathname === "/categories.html") {
+  categoryBtns.addEventListener("click", (e) => {
+    const clicked = e.target.closest(".category-btn");
+
+    if (!clicked) return;
+
+    categoryBtn.forEach((btn) => btn.classList.remove("active"));
+    categoriesContent.forEach((content) => content.classList.remove("active"));
+
+    clicked.classList.add("active");
+
+    document
+      .querySelector(`.categories-${clicked.dataset.tab}`)
+      .classList.add("active");
+  });
+}
